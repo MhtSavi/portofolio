05c33644df3bfb32cc7190c65abcf0bfe5b8c708
MhtSavi <hmhamat788@gmail.com>
2026-06-10 08:36:51 +0000

    Ajout du fichier Javascript

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const humburger = document.querySelector('.humburger');
+    const lienNavig = document.querySelector('.lien-nav');
+
+    humburger.addEventListener('click', function(){
+        lienNavig.classList.toggle('afficher');
+    });
+     
+    const liens = document.querySelectorAll('.lien-nav a');
+    liens.forEach(lien => {
+        lien.addEventListener('click', function(){
+            lienNavig.classList.remove('afficher');
+        });
+    });
+
+    const form = document.getElementById('contact-form');
+    if(form) {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            const nom = document.querySelector('#contact-form input[type="text"]');
+            const email = document.querySelector('#contact-form input[type="email"]');
+            const message = document.querySelector('#contact-form textarea');
+
+            let erreurs= [];
+            if(!nom.value.trim()) erreurs.push("Veuillez entrer votre nom.");
+            if(!email.value.trim() || !email.value.includes('@')) 
+                erreurs.push('Email invalide');
+            if(!message.value.trim()) erreurs.push("Veuillez entrer un message");
+            
+            if (erreurs.length > 0) {
+                alert("Message envoye. Merci !");
+                form.reset();
+            }
+        });
+    }
+});
